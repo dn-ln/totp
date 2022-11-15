afec871d772bc9ab74d4abdaed5b6a3d1925fff1
Dean Lin <richer6605@gmail.com>
2022-11-15 18:28:21 +0800

    Generate TOTP output continuously until interrupted

--- main.go
+++ main.go
@@ -3,11 +3,28 @@ package main
 import (
 	"crypto/sha1"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/ad-astra-9t/totp/totp"
 )
 
 func main() {
-	password := totp.GenerateTOTP([]byte("my-secret"), sha1.New, 6, 30)
-	fmt.Printf("Your OTP: %s\n", password)
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT)
+
+	tick := time.Tick(time.Second * 5)
+
+	fmt.Println("Start generating TOTP.")
+	for {
+		select {
+		case <-done:
+			return
+		case t := <-tick:
+			password := totp.GenerateTOTP([]byte("my-secret"), sha1.New, 6, 30)
+			fmt.Printf("Unix time: %v, Your OTP: %s\n", t.Unix(), password)
+		}
+	}
 }
